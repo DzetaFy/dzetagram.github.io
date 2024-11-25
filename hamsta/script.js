f79e9dbe939f61133c26f0273773488f7fb7f5ea
DzetaFy <dzetagramteam@gmail.com>
2024-11-25 16:19:12 +0100

    Update script.js

--- hamsta/script.js
+++ hamsta/script.js
@@ -1,9 +1,14 @@
-let score = 0;
+let score = parseInt(localStorage.getItem('dzetaScore')) || 0; // Загружаем сохраненный счет или устанавливаем 0
 
 const counterElement = document.querySelector('.counter');
 const dzetaButton = document.getElementById('dzeta-button');
 
+// Отображаем сохраненный счет
+counterElement.textContent = score;
+
+// Добавляем обработчик кликов
 dzetaButton.addEventListener('click', () => {
   score++;
   counterElement.textContent = score;
+  localStorage.setItem('dzetaScore', score); // Сохраняем счет в LocalStorage
 });
